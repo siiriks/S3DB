d92301351fdf34b8064931b2db75088ed3db117c
siiriks <159910517+siiriks@users.noreply.github.com>
2024-06-26 08:56:55 +0200

    Delete psychology directory

--- psychology/prochazka2022_2/preproc/preproc.R
+++
@@ -1,6 +0,0 @@
-
-library(haven)
-library(readr)
-
-dataset <- read_sav("psychology/prochazka2022_2/preproc/Pain_S2registered_Slovakia.sav")
-write_csv(dataset, "psychology/prochazka2022_2/dataset.csv")
